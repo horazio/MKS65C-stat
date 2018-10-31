481e65c01badfe1fd36cc5cedb672828c44dab83
horazio <hfusco@stuy.edu>
2018-10-30 21:36:10 -0400

    started processing epoch time a file is accessed into month and day

--- readme.c
+++ readme.c
@@ -13,6 +13,24 @@ int main(){
     printf("\nfailure\n");
   }
   int size = s -> st_size;
-  printf("%d", size);
+  double time = s -> st_atime;
+  int mode = s -> st_mode;
+
+  int base_time = 1540872000;
+  char * month = "Nov";
+  char * year = "2018";
+  int day = (time - base_time) / (3600 * 24);
+  char * week[7] = {"Thu", "Fri", "Sat", "Sun", "Mon", "Tue", "Wed"};
+  char * dayweek = "Wed";
+  if(day < 2){
+    day = 31;
+    month = "Oct";
+  }else{
+    day -= 2;
+    dayweek = week[day % 7];
+    day++;
+  }
+  
+  printf("%s %s %d\n\n", dayweek, month, day);
   return 0;
 }
